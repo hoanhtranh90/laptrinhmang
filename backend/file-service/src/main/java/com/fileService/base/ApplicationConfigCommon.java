1d846b3dcac937a0b6e4ff45ef812501382f0f0e
hoanhtranh80 <nguyenkhacsang100@gmail.com>
2022-05-27 00:02:32 +0700

    ed

--- backend/file-service/src/main/java/com/fileService/base/ApplicationConfigCommon.java
+++ backend/file-service/src/main/java/com/fileService/base/ApplicationConfigCommon.java
@@ -1,82 +1,82 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.fileService.base;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Component;
-
-/**
- *
- * @author sadfsafbhsaid
- */
-@Configuration
-public class ApplicationConfigCommon {
-
-     private static final Resource[] DEV_PROPERTIES
-            = new ClassPathResource[]{new ClassPathResource("application-dev.properties"),};
-
-    private static final Resource[] TEST_PROPERTIES
-            = new FileSystemResource[]{new FileSystemResource("./config/test-env.properties"),};
-
-//  private static final Resource[] PROD_PROPERTIES =
-//      new ClassPathResource[] {new ClassPathResource("prod-env.properties"),};
-    private static final Resource[] PROD_PROPERTIES
-            = new FileSystemResource[]{new FileSystemResource("./config/prod-env.properties"),};
-
-    @Profile("dev")
-    public static class DevConfig {
-
-        private DevConfig() {
-
-        }
-
-        @Bean
-        public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-            PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-            pspc.setLocations(DEV_PROPERTIES);
-            return pspc;
-        }
-    }
-
-    @Component
-    @Profile("test")
-    public static class TestConfig {
-
-        private TestConfig() {
-
-        }
-
-        @Bean
-        public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-            PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-            pspc.setLocations(TEST_PROPERTIES);
-
-            return pspc;
-        }
-    }
-
-    @Component
-    @Profile("prod")
-    public static class ProdConfig {
-
-        private ProdConfig() {
-        }
-
-        @Bean
-        public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-            PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-            pspc.setLocations(PROD_PROPERTIES);
-
-            return pspc;
-        }
-    }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package com.fileService.base;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.core.io.FileSystemResource;
+//import org.springframework.core.io.Resource;
+//import org.springframework.stereotype.Component;
+//
+///**
+// *
+// * @author sadfsafbhsaid
+// */
+//@Configuration
+//public class ApplicationConfigCommon {
+//
+//     private static final Resource[] DEV_PROPERTIES
+//            = new ClassPathResource[]{new ClassPathResource("application-dev.properties"),};
+//
+//    private static final Resource[] TEST_PROPERTIES
+//            = new FileSystemResource[]{new FileSystemResource("./config/test-env.properties"),};
+//
+////  private static final Resource[] PROD_PROPERTIES =
+////      new ClassPathResource[] {new ClassPathResource("prod-env.properties"),};
+//    private static final Resource[] PROD_PROPERTIES
+//            = new FileSystemResource[]{new FileSystemResource("./config/prod-env.properties"),};
+//
+//    @Profile("dev")
+//    public static class DevConfig {
+//
+//        private DevConfig() {
+//
+//        }
+//
+//        @Bean
+//        public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+//            PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+//            pspc.setLocations(DEV_PROPERTIES);
+//            return pspc;
+//        }
+//    }
+//
+//    @Component
+//    @Profile("test")
+//    public static class TestConfig {
+//
+//        private TestConfig() {
+//
+//        }
+//
+//        @Bean
+//        public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+//            PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+//            pspc.setLocations(TEST_PROPERTIES);
+//
+//            return pspc;
+//        }
+//    }
+//
+//    @Component
+//    @Profile("prod")
+//    public static class ProdConfig {
+//
+//        private ProdConfig() {
+//        }
+//
+//        @Bean
+//        public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+//            PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+//            pspc.setLocations(PROD_PROPERTIES);
+//
+//            return pspc;
+//        }
+//    }
+//}
